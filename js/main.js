178c6f7202551b582fb63219bc1adae50904a24b
Vu Hung <hung2811.iuh@gmail.com>
2020-05-08 10:24:43 +0700

    ok

--- js/main.js
+++ js/main.js
@@ -21,7 +21,6 @@ var pc = new RTCPeerConnection(servers);
 var objectData = {};
 pc.onicecandidate = (event => event.candidate ? 
   (function () {
-    console.log('ok');
     objectData.sender = yourId;
     objectData.ice = JSON.stringify(event.candidate);
     objectData.sdp = JSON.stringify(pc.localDescription);
